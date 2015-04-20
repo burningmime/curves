0d8b042a6a4578967900e36f5e6d06e7d4be2e1b
burningmime <fraser@burningmime.com>
2015-04-19 20:27:40 -0400

    Added project configurations for each vector type

--- runtests.bat
+++ runtests.bat
@@ -1,21 +1,26 @@
 @echo off
-set PERFTEST_EXE="%~dp0\bin\release\burningmime.curves.perftest.exe"
-if not exist "%PERFTEST_EXE%" (
-    echo Could not find file "%PERFTEST_EXE%"
-    goto END
+setlocal
+
+rem find path of test executable
+set PERFTEST_EXE="%~dp0\bin\release (system.numerics)\burningmime.curves.perftest.exe"
+if not exist %PERFTEST_EXE% (
+    echo Could not find file %PERFTEST_EXE%
+    goto end
 )
 
-setlocal
+rem old jit
 set COMPLUS_AltJit=
 set COMPLUS_FeatureSIMD=0
-"%PERFTEST_EXE%"
+%PERFTEST_EXE%
 
+rem new jit, simd off
 set COMPLUS_AltJit=*
 set COMPLUS_FeatureSIMD=0
-"%PERFTEST_EXE%"
+%PERFTEST_EXE%
 
+rem new jit, simd on
 set COMPLUS_AltJit=*
 set COMPLUS_FeatureSIMD=1
-"%PERFTEST_EXE%"
+%PERFTEST_EXE%
 
-:END
+:end
